cb75b5c372c3ea09a4b17963e534bb1c1b8beaf4
2dam <2dam@HZ359575.TartangaLH.eus>
2020-12-17 13:34:16 +0100

    error

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from . import models
+from . import artist
+from . import client
+from . import club
+from . import event
+from . import rating
+from . import user
--- models/event.py
+++ models/event.py
@@ -2,7 +2,7 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-from odoo import models
+from odoo import models, fields, api
 class Event(models.Model):
     _name = 'music_events.event'
     
