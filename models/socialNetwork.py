5bbf200856770ca72ba58fc8e61015c23b59e29d
aitorfi <aitorfidalgo@gmail.com>
2021-01-28 09:04:47 +0100

    Author comment added to model classes.

--- models/socialNetwork.py
+++ models/socialNetwork.py
@@ -2,6 +2,7 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
+# Ander Vicente
 from odoo import models,fields,api
 class SocialNetwork(models.Model):
     _name='music_events.socialnetwork'
